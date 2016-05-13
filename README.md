19ff88c0557dba2891b263d1390d4f509a2d30ac
Chang, Hao <popodidi@livemail.tw>
2016-05-13 10:26:38 +0800

    1.2.0

--- README.md
+++ README.md
@@ -1,12 +1,70 @@
 # gitbook-plugin-signature
 
-## book.json
+This is a plugin for automatically adding timestamp and copyright terms at the top and the bottom of every article. For now, it is using body font and you can specify the color, copyright owner and author of each article.
+
+Feel free to create issues for any discussion.
+
+## Usage
+
+### book.json
+
+I am not sure why it goes wrong when you don't specify the url. However, it still works with this setting as we put specific url
 
 ```json
 "plugins": ["gitbook-plugin-signature@git+https://github.com/popodidi/gitbook-plugin-signature.git"]
 ```
 
-## Usage
+```json
+"pluginsConfig": {
+      "gitbook-plugin-signature":{
+          "timeStampFormat" : "YYYY/MM/DD HH:mm:ss",
+          "autoTimeStamp":{
+            "color":"gray",
+            "author":"Hao",
+            "timeStampFormat": "YYYY/MM/DD HH:mm:ss"
+          },
+          "copyright":{
+              "color":"gray",
+               "owner":"Hao",
+               "center":true
+           }
+      }
+  }
+```
+
+`autoTimeStamp.author` and `copyright.owner` will be set to `book.author` if not specified.<br>
+If you don't want to automatically add copyright, simply drop the part in `pluginsConfig` as follows
+
+```json
+"pluginsConfig": {
+      "gitbook-plugin-signature":{
+          "timeStampFormat" : "YYYY/MM/DD HH:mm:ss",
+          "autoTimeStamp":{
+            "color":"gray",
+            "timeStampFormat": "YYYY/MM/DD HH:mm:ss"
+          }
+      }
+  }
+```
+
+Note that I don't put `author` in `autoTimeStamp`. As I mentioned, it will be set to `book.author` automatically.
+
+### Set Author for Each Article
+
+I tend to put settings in `book.json` rather than using filters as in the previous version. Only when you want to set author for each article, add this line of code at the top of the article
+
+```
+{% set author = "Hao" %}
+# Title of the Article
+
+content
+.
+.
+```
+
+## Deprecated Usage
+
+These methods are deprecated and will be discarded in the next version. So will the `pluginsConfig['gitbook-plugin-signature'].timeStampFormat`, which specifies the format of `**lastModifiedTimestamp**`.
 
 ### created/modified
 
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 const moment = require('moment');
-const debug = require('debug')('gitbook-plugin-signature-footer');
+const debug = require('debug')('gitbook-plugin-signature');
 /**
  * [main module]
  * @type {Object}
@@ -61,8 +61,68 @@ module.exports = {
                 'format': 'YYYY/MM/DD',
                 'utcOffset': '8'
             };
-            const timeStamp = ' <font color=\"gray\">{{ file.mtime | dateFormat("' + defaultOption.format + '", ' + defaultOption.utcOffset + ') }}</font><br><br>';
+            var color = 'gray';
+
+            // time stamp
+            var format = defaultOption.format;
+            if (this.config.options.pluginsConfig['gitbook-plugin-signature'].timeStampFormat) {
+                format = this.config.options.pluginsConfig['gitbook-plugin-signature'].timeStampFormat;
+            }
+            const timeStamp = ' <font color=\"gray\">{{ file.mtime | dateFormat("' + format + '", ' + defaultOption.utcOffset + ') }}</font>';
             page.content = page.content.replace('**lastModifiedTimestamp**', timeStamp);
+
+
+            // autoTimeStamp
+            if (this.config.options.pluginsConfig['gitbook-plugin-signature'].autoTimeStamp) {
+                var author = this.config.options.author;
+                var timeStampFormat = defaultOption.format;
+
+                // if (this.config.options.pluginsConfig['gitbook-plugin-signature'].autoTimeStamp.author){
+                //   author = this.config.options.pluginsConfig['gitbook-plugin-signature'].autoTimeStamp.author;
+                // }
+                if (this.config.options.pluginsConfig['gitbook-plugin-signature'].autoTimeStamp.timeStampFormat) {
+                    timeStampFormat = this.config.options.pluginsConfig['gitbook-plugin-signature'].autoTimeStamp.timeStampFormat;
+                }
+                if (this.config.options.pluginsConfig['gitbook-plugin-signature'].autoTimeStamp.color) {
+                    color = this.config.options.pluginsConfig['gitbook-plugin-signature'].autoTimeStamp.color;
+                }
+
+                const colorLeft = ' <font color=\"' + color + '\">';
+                var autolastModified = colorLeft + 'last modified by ' + author;
+                const autoTimeStamp = '{{ file.mtime | dateFormat("' + timeStampFormat + '", ' + defaultOption.utcOffset + ') }}</font>';
+
+                if (page.content.indexOf('{% set author') != -1) {
+                    author = '{{author}}';
+                    autolastModified = colorLeft + 'last modified by ' + author;
+                    page.content = page.content.slice(0, (page.content.indexOf('%}\n')+2))
+                                    + autolastModified + ', ' + autoTimeStamp + '\n\n'
+                                    + page.content.slice(page.content.indexOf('%}\n')+2);
+                }else{
+                  page.content = autolastModified + ', ' + autoTimeStamp + '\n\n' + page.content;
+                }
+            }
+
+            // auto copyright
+            if (this.config.options.pluginsConfig['gitbook-plugin-signature'].copyright) {
+                var owner = this.config.options.author;
+                const year = '{{ file.mtime | dateFormat("' + 'YYYY' + '", ' + defaultOption.utcOffset + ') }}';
+                var centerLeft, centerRight = '';
+
+                if (this.config.options.pluginsConfig['gitbook-plugin-signature'].copyright.owner) {
+                    owner = this.config.options.pluginsConfig['gitbook-plugin-signature'].copyright.owner;
+                }
+                if (this.config.options.pluginsConfig['gitbook-plugin-signature'].copyright.center) {
+                    centerLeft = '<center>';
+                    centerRight = '</center>';
+                }
+                if (this.config.options.pluginsConfig['gitbook-plugin-signature'].copyright.color) {
+                    color = this.config.options.pluginsConfig['gitbook-plugin-signature'].copyright.color;
+                }
+
+                const colorLeft = ' <font color=\"' + color + '\">';
+
+                page.content = page.content + '\n\n\n\n<br><br>' + centerLeft + colorLeft + 'Copyright © ' + year + ' ' + owner + '.' + '<br>All rights reserved.</font>' + centerRight;
+            }
             return page;
         }
     }
